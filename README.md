77e12e2a80c3a81ec836e99b3a5f953598ee1829
Surawu <surawu@outlook.com>
2021-03-18 13:52:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-acxd2f
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-acxd2f)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, VERSION, AfterViewInit, ViewChild } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements AfterViewInit {
+  name = "Angular " + VERSION.major;
+  constructor() {}
+
+  ngAfterViewInit() {}
+}
