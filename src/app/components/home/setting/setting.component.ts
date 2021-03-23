e207dad546d8a79499b1a018199f5b9f07260f00
Surawu <surawu@outlook.com>
2021-03-23 15:10:47 +0800

    1 file modified

--- src/app/components/home/setting/setting.component.ts
+++ src/app/components/home/setting/setting.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
 
 @Component({
-  selector: 'app-setting',
-  templateUrl: './setting.component.html',
-  styleUrls: ['./setting.component.css']
+  selector: "app-setting",
+  templateUrl: "./setting.component.html",
+  styleUrls: ["./setting.component.css"]
 })
 export class SettingComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
+  public name: string;
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      console.log(params);
+      this.name = params["name"];
+    });
   }
-
 }
